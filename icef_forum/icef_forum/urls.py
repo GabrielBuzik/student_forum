aa58d055bb82b158add2e092beaecbd809365773
Gabriel Buzukashvili <gabriel.bazak@gmail.com>
2023-05-19 19:34:04 +0300

    media

--- icef_forum/icef_forum/urls.py
+++ icef_forum/icef_forum/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
